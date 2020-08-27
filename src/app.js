25e41dd463c91df2e3dddcc86c38c97f244630cb
ysz951 <shengyang_zhou@baylor.edu>
2020-08-27 01:58:50 -0500

    change cors

--- src/app.js
+++ src/app.js
@@ -17,11 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
--- src/config.js
+++ src/config.js
@@ -5,5 +5,4 @@ module.exports = {
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://dontforget_server@localhost/dontforget-test",
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://dont-forget-app.ysz951.vercel.app/",
   };
