0f12e34ada7e63461c144bac936d3d54ad317659
ysz951 <shengyang_zhou@baylor.edu>
2020-08-27 01:54:44 -0500

    add cors config

--- src/app.js
+++ src/app.js
@@ -6,9 +6,8 @@ const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const buyListsRouter = require('./buyLists/buyLists-router');
 const nextListsRouter = require('./nextLists/nextLists-router');
-// const shoppingListsRouter = require('./shoppingLists/shoppingLists-router');
 const itemsRouter= require('./items/items-router');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
@@ -18,14 +17,17 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 });
 app.use('/api/buylists', buyListsRouter);
 app.use('/api/nextlists', nextListsRouter);
-// app.use('/api/shopping', shoppingListsRouter);
 app.use('/api/items', itemsRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
